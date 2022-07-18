89a17383d7bc7fb767ff470a9616bdbca067a5e4
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-07-18 15:18:10 +0200

    feat: AOC 2015-05 part 2

--- 2015/aoc-2015-05/src/main.rs
+++ 2015/aoc-2015-05/src/main.rs
@@ -1,6 +1,7 @@
 use fancy_regex::Regex;
 use std::fs;
 
+// Part 1
 fn is_nice_string(text: &str) -> bool {
     let other_than_vowels = Regex::new(r"(.*[aeiou]){3}").unwrap();
     let contains_bad_parts = Regex::new(r"(ab|cd|pq|xy)").unwrap();
@@ -11,16 +12,38 @@ fn is_nice_string(text: &str) -> bool {
         && !contains_bad_parts.is_match(text).unwrap();
 }
 
+// Part 2
+fn is_nice_string_2(text: &str) -> bool {
+    let contains_repeated_letter_with_break = Regex::new(r"(.{1}).{1}(\1)").unwrap();
+    let contains_repeated_pair = Regex::new(r"(.{2}).*\1{1,}").unwrap();
+
+    return contains_repeated_letter_with_break.is_match(text).unwrap()
+        && contains_repeated_pair.is_match(text).unwrap();
+}
+
 fn main() {
     let file = "./input.txt";
     let input = fs::read_to_string(file).unwrap();
-    let amount_of_nice_strings = input
-        .split("\n")
-        .filter(|l| !l.is_empty() && is_nice_string(l))
+    let lines = input.split("\n").filter(|l| !l.is_empty());
+
+    let amount_of_nice_strings = lines
+        .clone()
+        .filter(|l| is_nice_string(l))
         .collect::<Vec<_>>()
         .len();
 
     println!("Amount of nice strings: {}", amount_of_nice_strings);
+
+    let amount_of_nice_strings_2 = lines
+        .clone()
+        .filter(|l| is_nice_string_2(l))
+        .collect::<Vec<_>>()
+        .len();
+
+    println!(
+        "Amount of nice strings part 2: {}",
+        amount_of_nice_strings_2
+    );
 }
 
 #[cfg(test)]
@@ -50,5 +73,26 @@ mod test {
     #[test]
     fn it_returns_false_for_string_with_only_one_vowel() {
         assert!(!is_nice_string("dvszwmarrgswjxmb"));
+    }
+
+    // Part 2
+    #[test]
+    fn it_returns_true_for_repeated_pair() {
+        assert!(is_nice_string_2("qjhvhtzxzqqjkmpb"));
+    }
+
+    #[test]
+    fn it_returns_true_for_letter_with_break() {
+        assert!(is_nice_string_2("xxyxx"));
+    }
+
+    #[test]
+    fn it_returns_false_no_repeated_letter() {
+        assert!(!is_nice_string_2("uurcxstgmygtbstg"));
+    }
+
+    #[test]
+    fn it_returns_false_no_repeated_pair() {
+        assert!(!is_nice_string_2("ieodomkazucvgmuy"));
     }
 }
