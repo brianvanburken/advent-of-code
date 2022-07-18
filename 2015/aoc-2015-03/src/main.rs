79df41986b8db6a8b54d6291dcf1693992b4f0f0
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-07-18 13:26:17 +0200

    feat: AOC 2015-03 part 2

--- 2015/aoc-2015-03/src/main.rs
+++ 2015/aoc-2015-03/src/main.rs
@@ -1,22 +1,38 @@
 use std::collections::HashSet;
 use std::fs;
 
+// Part 1
 fn calculate_unique_delivered_houses(sequence: &str) -> usize {
-    let mut current_x = 0;
-    let mut current_y = 0;
-    let start_coordinate = format!("{},{}", current_x, current_y);
-    let mut visited_coordinates = HashSet::from([start_coordinate]);
+    let mut pos = (0, 0);
+    let mut visited_coordinates = HashSet::from([pos]);
 
     for movement in sequence.chars() {
         match movement {
-            '>' => current_x += 1,
-            '<' => current_x -= 1,
-            '^' => current_y += 1,
-            'v' => current_y -= 1,
+            '^' => pos = (pos.0, pos.1 - 1),
+            '>' => pos = (pos.0 + 1, pos.1),
+            '<' => pos = (pos.0 - 1, pos.1),
+            'v' => pos = (pos.0, pos.1 + 1),
             _ => (),
         }
-        let coordinate = format!("{},{}", current_x, current_y);
-        visited_coordinates.insert(coordinate);
+        visited_coordinates.insert(pos);
+    }
+    visited_coordinates.len()
+}
+
+// Part 2
+fn calculate_robo_and_santa_deliveries(sequence: &str) -> usize {
+    let mut pos = [(0, 0), (0, 0)];
+    let mut visited_coordinates = HashSet::from([pos[0]]);
+
+    for (i, movement) in sequence.chars().enumerate() {
+        match movement {
+            '^' => pos[i % 2] = (pos[i % 2].0, pos[i % 2].1 - 1),
+            '>' => pos[i % 2] = (pos[i % 2].0 + 1, pos[i % 2].1),
+            '<' => pos[i % 2] = (pos[i % 2].0 - 1, pos[i % 2].1),
+            'v' => pos[i % 2] = (pos[i % 2].0, pos[i % 2].1 + 1),
+            _ => (),
+        }
+        visited_coordinates.insert(pos[i % 2]);
     }
     visited_coordinates.len()
 }
@@ -24,13 +40,16 @@ fn calculate_unique_delivered_houses(sequence: &str) -> usize {
 fn main() {
     let file = "./input.txt";
     let input = fs::read_to_string(file).unwrap();
-    let all_visited_houses = input
-        .split("\n")
-        .filter(|l| !l.is_empty())
-        .map(|sequence| calculate_unique_delivered_houses(sequence))
-        .sum::<usize>();
+    let sequence = input.trim();
 
-    println!("Amount of visited houses: {}", all_visited_houses)
+    let all_visited_houses = calculate_unique_delivered_houses(sequence);
+    println!("Amount of visited houses by santa: {}", all_visited_houses);
+
+    let both_visited_houses = calculate_robo_and_santa_deliveries(sequence);
+    println!(
+        "Amount of visited houses by santa and robo: {}",
+        both_visited_houses
+    );
 }
 
 #[cfg(test)]
@@ -50,5 +69,21 @@ mod test {
     #[test]
     fn it_returns_2_for_repeated_same_houses() {
         assert_eq!(calculate_unique_delivered_houses("^v^v^v^v^v"), 2);
+    }
+
+    // Part 2
+    #[test]
+    fn it_returns_3_for_2_steps() {
+        assert_eq!(calculate_robo_and_santa_deliveries("^v"), 3);
+    }
+
+    #[test]
+    fn it_returns_3_for_both_returning_to_same_house() {
+        assert_eq!(calculate_robo_and_santa_deliveries("^>v<"), 3);
+    }
+
+    #[test]
+    fn it_returns_11_for_both_going_other_direction() {
+        assert_eq!(calculate_robo_and_santa_deliveries("^v^v^v^v^v"), 11);
     }
 }
