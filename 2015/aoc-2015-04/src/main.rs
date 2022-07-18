f7aa92414afc089d511001d39a4a61ea79a4def9
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-07-18 14:18:32 +0200

    feat: AOC 2015-04 part 2

--- 2015/aoc-2015-04/src/main.rs
+++ 2015/aoc-2015-04/src/main.rs
@@ -2,11 +2,13 @@ use std::convert::TryInto;
 
 use md5;
 
-fn calculate_coin_number_for_secret(secret: &str) -> usize {
+fn calculate_coin_number_for_secret(secret: &str, start_len: usize) -> usize {
+    let start_hash_match = "0".repeat(start_len);
+
     for i in 1..i64::MAX {
         let digest = md5::compute(secret.to_owned() + &i.to_string());
         let hash = format!("{:x}", digest);
-        if hash.starts_with("00000") {
+        if hash.starts_with(&start_hash_match) {
             return i.try_into().unwrap();
         }
     }
@@ -15,8 +17,13 @@ fn calculate_coin_number_for_secret(secret: &str) -> usize {
 
 fn main() {
     println!(
-        "Result for secret: {}",
-        calculate_coin_number_for_secret("ckczppom")
+        "Result for secret part 1: {}",
+        calculate_coin_number_for_secret("ckczppom", 5)
+    );
+
+    println!(
+        "Result for secret part 2: {}",
+        calculate_coin_number_for_secret("ckczppom", 6)
     );
 }
 
@@ -26,11 +33,11 @@ mod test {
 
     #[test]
     fn it_returns_609043_for_secret_abcdef() {
-        assert_eq!(calculate_coin_number_for_secret("abcdef"), 609043);
+        assert_eq!(calculate_coin_number_for_secret("abcdef", 5), 609043);
     }
 
     #[test]
     fn it_returns_1048970_for_secret_pqrstuv() {
-        assert_eq!(calculate_coin_number_for_secret("pqrstuv"), 1048970);
+        assert_eq!(calculate_coin_number_for_secret("pqrstuv", 5), 1048970);
     }
 }
