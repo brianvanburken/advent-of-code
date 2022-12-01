bb4358785e7506bb84ff14429380730405792103
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-12-01 14:25:49 +0100

    feat: AoC 2022-01 part 1 & 2

---
+++ 2022/aoc-2022-01/src/main.rs
@@ -0,0 +1,46 @@
+use std::fs;
+
+fn main() {
+    let data = fs::read_to_string("./input.txt").expect("Unable to read file");
+
+    // Part 1
+    let mut highest_total = 0;
+    let mut current_elf_calories = 0;
+
+    for line in data.lines().into_iter() {
+        if line.trim() == "" {
+            highest_total = if current_elf_calories > highest_total {
+                current_elf_calories
+            } else {
+                highest_total
+            };
+            current_elf_calories = 0;
+        } else {
+            let calories = line.parse::<usize>().unwrap();
+            current_elf_calories += calories;
+        }
+    }
+
+    println!("Part 1: {:?}", highest_total);
+
+    // Part 2
+    let mut totals: Vec<usize> = vec![];
+    let mut current_elf_calories = 0;
+
+    for line in data.lines().into_iter() {
+        if line.trim() == "" {
+            totals.push(current_elf_calories);
+            current_elf_calories = 0;
+        } else {
+            let calories = line.parse::<usize>().unwrap();
+            current_elf_calories += calories;
+        }
+    }
+
+    totals.sort();
+    totals.reverse();
+
+    let top_3: usize = totals[0..3].iter().sum();
+
+    println!("Part 2: {:?}", top_3);
+}
