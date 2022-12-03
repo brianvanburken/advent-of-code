304d09bc6e51e4f6e72c6eb1e3a1b6a26d34ebbc
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-12-03 22:25:15 +0100

    feat: Rewrote 2022-03 with iterators

--- 2022/aoc-2022-03/src/main.rs
+++ 2022/aoc-2022-03/src/main.rs
@@ -10,38 +10,45 @@ fn main() {
         .map(|(index, chr)| (chr, index + 1))
         .collect::<HashMap<char, usize>>();
 
-    let mut total_score_part_1 = 0;
-    for line in input.lines().into_iter() {
-        let compartment_length = line.len() / 2;
-        let compartment_a = &line[0..compartment_length];
-        let compartment_b = &line[compartment_length..];
+    let part_1 = input
+        .lines()
+        .into_iter()
+        .map(|line| {
+            let compartment_length = line.len() / 2;
+            let compartment_a = &line[0..compartment_length];
+            let compartment_b = &line[compartment_length..];
 
-        let matched_common_char = compartment_a
-            .chars()
-            .find(|&c| compartment_b.contains(c))
-            .expect("Expected a common char in string");
-
-        total_score_part_1 += letter_priorities
-            .get(&matched_common_char)
-            .expect("Expected letter priority to be set for char");
-    }
-    println!("Part 1: {:?}", total_score_part_1);
-
-    let mut total_score_part_2 = 0;
-    let mut lines = Vec::with_capacity(3);
-    for line in input.lines() {
-        lines.push(line);
-        if lines.len() == 3 {
-            let matched_common_char = lines[0]
+            let matched_common_char = compartment_a
                 .chars()
-                .find(|&c| lines[1].contains(c) && lines[2].contains(c))
+                .find(|&c| compartment_b.contains(c))
+                .expect("Expected a common char in string");
+
+            letter_priorities
+                .get(&matched_common_char)
+                .expect("Expected letter priority to be set for char")
+        })
+        .sum::<usize>();
+
+    println!("Part 1: {:?}", part_1);
+
+    let part_2 = input
+        .lines()
+        .collect::<Vec<&str>>()
+        .chunks(3)
+        .map(|chunk| {
+            let [a, b, c] = chunk else {
+                panic!("Expected chunk of 3")
+            };
+            let matched_common_char = a
+                .chars()
+                .find(|&chr| b.contains(chr) && c.contains(chr))
                 .expect("Expected a common char in strings");
 
-            total_score_part_2 += letter_priorities
+            *letter_priorities
                 .get(&matched_common_char)
-                .expect("Expected letter priority to be set for char");
-            lines.clear();
-        }
-    }
-    println!("Part 2: {:?}", total_score_part_2);
+                .expect("Expected letter priority to be set for char")
+        })
+        .sum::<usize>();
+
+    println!("Part 2: {:?}", part_2);
 }
