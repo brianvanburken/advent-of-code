60c6543326534f0cda8939f03b7f7036edb716ff
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-12-03 21:03:17 +0100

    feat: AoC 2022-03 part 2

--- 2022/aoc-2022-03/src/main.rs
+++ 2022/aoc-2022-03/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
         .map(|(index, chr)| (chr, index + 1))
         .collect::<HashMap<char, usize>>();
 
-    let mut total_score = 0;
+    let mut total_score_part_1 = 0;
     for line in input.lines().into_iter() {
         let compartment_length = line.len() / 2;
         let compartment_a = &line[0..compartment_length];
@@ -21,9 +21,27 @@ fn main() {
             .find(|&c| compartment_b.contains(c))
             .expect("Expected a common char in string");
 
-        total_score += letter_priorities
+        total_score_part_1 += letter_priorities
             .get(&matched_common_char)
             .expect("Expected letter priority to be set for char");
     }
-    println!("Part 1: {:?}", total_score);
+    println!("Part 1: {:?}", total_score_part_1);
+
+    let mut total_score_part_2 = 0;
+    let mut lines = Vec::with_capacity(3);
+    for line in input.lines() {
+        lines.push(line);
+        if lines.len() == 3 {
+            let matched_common_char = lines[0]
+                .chars()
+                .find(|&c| lines[1].contains(c) && lines[2].contains(c))
+                .expect("Expected a common char in strings");
+
+            total_score_part_2 += letter_priorities
+                .get(&matched_common_char)
+                .expect("Expected letter priority to be set for char");
+            lines.clear();
+        }
+    }
+    println!("Part 2: {:?}", total_score_part_2);
 }
