92e6e8c83a4ea0c69153645a1d5fc3a905f471e0
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-12-04 10:21:16 +0100

    feat: AoC 2022-04 part 1

---
+++ 2022/aoc-2022-04/src/main.rs
@@ -0,0 +1,30 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("./input.txt").expect("Unable to read file");
+
+    let pairs: Vec<Vec<isize>> = input
+        .lines()
+        .map(|pair| {
+            pair.split(",")
+                .flat_map(|assignment| {
+                    assignment
+                        .split("-")
+                        .map(|section| section.parse().expect("Expected assignment to be a number"))
+                        .collect::<Vec<isize>>()
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    let mut sum_part_1 = 0;
+    for pair in pairs {
+        let [s1,e1,s2,e2] = &pair[..] else {
+                panic!("Expected a pair with assignment resulting in four numbers")
+            };
+        if (s1 <= s2 && e1 >= e2) || (s1 >= s2 && e1 <= e2) {
+            sum_part_1 += 1;
+        }
+    }
+    println!("Part 1: {:?}", sum_part_1);
+}
