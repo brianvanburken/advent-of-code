0edf9f7f1fc20fe347c7c74c8712bb5ba031d8eb
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-12-06 14:10:17 +0100

    feat: AoC 2022-06 part 2

--- 2022/aoc-2022-06/src/main.rs
+++ 2022/aoc-2022-06/src/main.rs
@@ -11,4 +11,12 @@ fn main() {
         .expect("Expected to find a marker!");
 
     println!("Part 1: {:?}", (start_of_marker + 4));
+
+    let start_of_marker = input
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(14)
+        .position(|window| HashSet::<&char>::from_iter(window.iter()).len() == window.len())
+        .expect("Expected to find a marker!");
+    println!("Part 2: {:?}", (start_of_marker + 14));
 }
