0d33fc73fca70b28d63df152f50813d6417f399a
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-12-01 18:37:52 +0100

    refactor(2023-01): clean up code and add tests

--- 2023/aoc-2023-01/src/main.rs
+++ 2023/aoc-2023-01/src/main.rs
@@ -7,23 +7,25 @@ const DIGITS: [&[u8]; 9] = [
 fn main() {
     let input = fs::read_to_string("./input.txt").expect("Unable to read input file");
 
-    let mut part1: u32 = 0;
-    for line in input.lines() {
-        let mut digits = line.chars().filter_map(|c| c.to_digit(10));
-        let first = digits.next().expect("Expect to have at least one number");
-        let last = digits.last().unwrap_or(first);
-        part1 += first * 10 + last;
-    }
-    println!("Part 1: {}", part1);
-
+    let mut part1 = 0;
     let mut part2 = 0;
     for line in input.lines() {
-        part2 += convert_to_double_digit(line);
+        part1 += line_to_digit(line);
+        part2 += spelled_line_to_digit(line);
     }
+
+    println!("Part 1: {}", part1);
     println!("Part 2: {}", part2);
 }
 
-fn convert_to_double_digit(i: &str) -> u32 {
+fn line_to_digit(line: &str) -> u32 {
+    let mut digits = line.chars().filter_map(|c| c.to_digit(10));
+    let first = digits.next().unwrap_or(0);
+    let last = digits.last().unwrap_or(first);
+    first * 10 + last
+}
+
+fn spelled_line_to_digit(i: &str) -> u32 {
     let mut first: u32 = 0;
     let mut last: u32 = 0;
 
@@ -38,9 +40,10 @@ fn convert_to_double_digit(i: &str) -> u32 {
             first = if first == 0 { digit * 10 } else { first };
             last = digit;
         } else {
-            for (index, spelled_digit) in DIGITS.iter().enumerate() {
-                if chars[i..].starts_with(spelled_digit) {
-                    let digit = (index + 1) as u32;
+            for (zero_based_index, spelled_out_digit) in DIGITS.iter().enumerate() {
+                println!("{}, {}", zero_based_index, chars[i..].starts_with(spelled_out_digit));
+                if chars[i..].starts_with(spelled_out_digit) {
+                    let digit = (zero_based_index + 1) as u32;
                     first = if first == 0 { digit * 10 } else { first };
                     last = digit;
                 }
@@ -50,4 +53,89 @@ fn convert_to_double_digit(i: &str) -> u32 {
     }
 
     first + last
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    // Part 1
+    #[test]
+    fn line_to_digit_returns_0_if_no_digits_found() {
+        assert_eq!(line_to_digit(""), 0);
+        assert_eq!(line_to_digit(" "), 0);
+        assert_eq!(line_to_digit("-"), 0);
+        assert_eq!(line_to_digit("a"), 0);
+        assert_eq!(line_to_digit("aa"), 0);
+    }
+
+    #[test]
+    fn line_to_digit_returns_first_digit_as_last_with_only_a_single_digit() {
+        assert_eq!(line_to_digit("7"), 77);
+        assert_eq!(line_to_digit("1"), 11);
+        assert_eq!(line_to_digit("3"), 33);
+    }
+
+    #[test]
+    fn line_to_digit_returns_digit_if_only_contains_two_digits() {
+        assert_eq!(line_to_digit("11"), 11);
+        assert_eq!(line_to_digit("21"), 21);
+        assert_eq!(line_to_digit("99"), 99);
+    }
+
+    #[test]
+    fn line_to_digit_returns_digit_if_contains_two_digits_with_letters() {
+        assert_eq!(line_to_digit("a99"), 99);
+        assert_eq!(line_to_digit("99a"), 99);
+        assert_eq!(line_to_digit("9a9"), 99);
+        assert_eq!(line_to_digit("a9a9"), 99);
+        assert_eq!(line_to_digit("9a9a"), 99);
+        assert_eq!(line_to_digit("a9a9a"), 99);
+    }
+
+    #[test]
+    fn line_to_digit_returns_first_and_last_if_contains_multiple_digits() {
+        assert_eq!(line_to_digit("101"), 11);
+        assert_eq!(line_to_digit("123"), 13);
+        assert_eq!(line_to_digit("90000009"), 99);
+    }
+
+    #[test]
+    fn line_to_digit_returns_first_and_last_if_contains_multiple_digits_with_letters() {
+        assert_eq!(line_to_digit("a919"), 99);
+        assert_eq!(line_to_digit("919a"), 99);
+        assert_eq!(line_to_digit("9a19"), 99);
+        assert_eq!(line_to_digit("a9a19"), 99);
+        assert_eq!(line_to_digit("9a19a"), 99);
+        assert_eq!(line_to_digit("a9a19a"), 99);
+        assert_eq!(line_to_digit("a9a1a9a"), 99);
+    }
+
+    #[test]
+    fn example_part1() {
+        assert_eq!(line_to_digit("1abc2"), 12);
+        assert_eq!(line_to_digit("pqr3stu8vwx"), 38);
+        assert_eq!(line_to_digit("a1b2c3d4e5f"), 15);
+        assert_eq!(line_to_digit("treb7uchet"), 77);
+    }
+
+    // Part 2
+    #[test]
+    fn spelled_line_to_digit_returns_0_if_not_found() {
+        assert_eq!(line_to_digit(""), 0);
+        assert_eq!(line_to_digit("a"), 0);
+        assert_eq!(line_to_digit("-"), 0);
+        assert_eq!(line_to_digit("zero"), 0);
+    }
+
+    #[test]
+    fn example_part2() {
+        assert_eq!(spelled_line_to_digit("two1nine"), 29);
+        assert_eq!(spelled_line_to_digit("eightwothree"), 83);
+        assert_eq!(spelled_line_to_digit("abcone2threexyz"), 13);
+        assert_eq!(spelled_line_to_digit("xtwone3four"), 24);
+        assert_eq!(spelled_line_to_digit("4nineeightseven2"), 42);
+        assert_eq!(spelled_line_to_digit("zoneight234"), 14);
+        assert_eq!(spelled_line_to_digit("7pqrstsixteen"), 76);
+    }
 }
