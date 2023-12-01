a5bbfb8708465102faef597b39fc8577785dfe3e
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-12-01 16:08:19 +0100

    refactor(2023-01): use for-loop and filter_map

--- 2023/aoc-2023-01/src/main.rs
+++ 2023/aoc-2023-01/src/main.rs
@@ -3,28 +3,13 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("./input.txt").expect("Unable to read input file");
 
-    let part1: u32 = input.lines().map(convert_to_double_digit).sum();
-
+    let mut part1: u32 = 0;
+    for line in input.lines() {
+        let mut digits = line.chars().filter_map(|c| c.to_digit(10));
+        let first = digits.next().expect("Expect to have at least one number");
+        let last = digits.last().unwrap_or(first);
+        part1 += first * 10 + last;
+    }
     println!("Part 1: {}", part1);
 }
 
-fn convert_to_double_digit(line: &str) -> u32 {
-    let numbers = line
-        .chars()
-        .filter(char::is_ascii_digit)
-        .collect::<String>();
-
-    let digit = format!(
-        "{}{}",
-        numbers
-            .chars()
-            .next()
-            .expect("Expect to have at least one number"),
-        numbers
-            .chars()
-            .last()
-            .expect("Expect to have a last number")
-    );
-
-    digit.parse::<u32>().expect("Expect a valid digit")
-}
